b15bd751ae0aeae58d7cb40ae07ec28ee7b7f551
quentingourier <73183451+quentingourier@users.noreply.github.com>
2020-12-05 17:46:09 +0100

    Add files via upload

--- tp3.py
+++ tp3.py
@@ -27,17 +27,17 @@ def MIGenerale ( M , N , b , x0 , epsilon , Nitermax ):
 def MIJacobi ( A , b , x0 , epsilon , Nitermax):
     M = np.diag(np.diag(A))
     N = M - A
-    print("\nDécomposition de Jacobi\n")
+    '''print("\nDécomposition de Jacobi\n")
     print("M =\n", M)
-    print("N =\n", N)
+    print("N =\n", N)'''
     return MIGenerale(M, N, b, x0, epsilon, Nitermax)
 
 def MIGaussSeidel ( A , b , x0 , epsilon , Nitermax):
     M = np.tril(A)
     N = M - A
-    print("\nDécomposition de Gauss-Seidel\n")
+    '''print("\nDécomposition de Gauss-Seidel\n")
     print("M =\n", M)
-    print("N =\n", N)
+    print("N =\n", N)'''
     return MIGenerale(M, N, b, x0, epsilon, Nitermax)
 
 def decompo_Relaxation(A, w):
@@ -79,7 +79,7 @@ def verif_conv(A, w):
     return ray_spec
     
 def w_etude(A):
-    w = np.linspace(0.1, 1.9, 1000)
+    w = np.linspace(0.1, 1.9, 10000)
     liste_vconv = []
     for i in range(len(w)):
         liste_vconv.append(verif_conv(A, w[i]))
@@ -106,22 +106,51 @@ def courbe_temps_w(liste_w, liste_tps):
     
 def courbe_conv_x(liste_w, liste_vconv):
     pp.plot(liste_w, liste_vconv, label = "rayon de convergence pour w", color = 'g')
-    pp.xlim(0.7, 1.9)
+    pp.xlim(0.7, 1.5)
     pp.xlabel('w')
     pp.ylabel("rayon de convergence")
     pp.title("rayon de convergence en fonction du paramètre w")
     pp.legend()
     pp.show()
 
+#----------FONCTIONS UTILES-----------------------------------------------------
+
+def gauss(A, B):
+    Aaug = np.concatenate((A, B), axis = 1)
+    Baug = rg(Aaug)
+    compteur = res(Baug)    
+    return compteur
+
+def rg(Aaug):
+    n,m = np.shape(Aaug) #retourne les lignes et colonnes (la dimension)
+    for k in range(0, n-1):
+        for i in range(k+1, n):
+            gik = Aaug[i, k]/Aaug[k,k]
+            Aaug[i, :] = Aaug[i, :] - gik*Aaug[k, :]
+    return Aaug
+
+def res(Aaug):
+    n, m = np.shape(Aaug)
+    X = np.zeros(n)
+    compteur = 0
+    for i in range(n-1, -1, -1): 
+        somme = 0
+        for k in range(i,n):
+            somme = somme + X[k]*Aaug[i,k]
+        X[i] = (Aaug[i, n] - somme) / Aaug[i, i]
+        compteur += 1
+    return compteur
+
+
 #----------------------------------------------------------------------------
 #-----------PROGRAMME---------------------------------------------------
 #----------------------------------------------------------------------------
   
-
+'''
 #question 1
 #voir la def
 
-'''
+
 A = np.array([[10, 1], [-1, 10]])
 b = np.array([1, 2])
 x0 = np.zeros(b.shape)
@@ -138,7 +167,6 @@ print("\nx = ", MIRelaxation ( A , b, w, x0 , epsilon , Nitermax), "\n")
 w = 1.5
 print(MIRelaxation ( A , b , w , x0 , epsilon , Nitermax))
 
-'''
 
 # Partie 2 Expérimentation des méthodes
 
@@ -152,13 +180,15 @@ for i in range (0,100):
             b[i,0] = cos((i+1)/8)
         else:
             A[i,j] = 1/(12+(3*(i+1)-5*(j+1))**2)
-verif_conv(A)   
+
+   
 b = np.asarray(b).reshape(-1)#remettre b sous forme de vecteur
 x0 = np.zeros(b.shape)
 Nitermax = 100
 liste_epsilon = [10**-4, 10**-5, 10**-6, 10**-7, 10**-8, 10**-9, 10**-10, 10**-11, 10**-12, 10**-13, 10**-14]
 liste_nb_iteration_J = []
 liste_nb_iteration_GS = []
+
 for i in range(len (liste_epsilon)):
     x, nbiter, err = MIJacobi(A, b, x0, liste_epsilon[i], Nitermax)
     liste_nb_iteration_J.append(nbiter)
@@ -166,6 +196,7 @@ for i in range(len (liste_epsilon)):
     liste_nb_iteration_GS.append(nbiter)
 print(liste_nb_iteration_J)
 print(liste_nb_iteration_GS)
+
 pp.gca().invert_xaxis()
 pp.plot(liste_epsilon, liste_nb_iteration_J, label = 'Méthode Jacobi')
 pp.plot(liste_epsilon,liste_nb_iteration_GS, label ='Méthode de Gauss-Seidel')
@@ -188,7 +219,6 @@ for i in range (0,100):
             b[i,0] = cos((i+1)/8)
         else:
             A[i,j] = 1/(1+3*abs((i+1)-(j+1)))
-verif_conv(A)
 x0 = np.zeros((100,1))
 liste_epsilon = [10**-4, 10**-5, 10**-6, 10**-7, 10**-8, 10**-9, 10**-10, 10**-11, 10**-12, 10**-13, 10**-14]
 Nitermax = 100
@@ -209,9 +239,10 @@ pp.title("Nombre d'itération nécessaires pour plusieurs précisions souhaitée
 pp.legend()
 pp.show()
 
-
+'''
 #question 3
-n= 100
+
+'''
 #----SYSTEME 1-------------------------------------------------
 A = np.zeros((n,n)) #SYSTEME 1
 b = np.zeros((n, 1))
@@ -274,9 +305,49 @@ w_etude(A)
 #ETUDE W OPTIMAL
 A = np.array([[2, 1, 0], [1, 2, 1], [0, 1, 2]])
 w_etude(A)
+'''
 
+#comparaison méthode iteratives / directes
+taille = [100]
+liste_nb_iteration_J = []
+liste_nb_iteration_GS = []
+liste_nb_iteration_Gauss = []
+epsilon = 10**-13
+Nitermax = 1000
+for i in range(len(taille)):
+    n = taille[i]
+    A = np.zeros((n,n))
+    b = np.zeros((n,1))
+    for i in range(n):
+        for j in range(n):
+            if i == j:
+                A[i, j] = 3
+                b[i,0] = cos((i+1)/8)
+            else:
+                    A[i,j] = 1/(1+3*abs((i+1)-(j+1)))
+    x0 = np.zeros((n,1))    
+    x , nbiter , err = MIJacobi(A, b, x0, epsilon, Nitermax)
+    liste_nb_iteration_J.append(nbiter)
+    x , nbiter , err = MIGaussSeidel(A, b, x0, epsilon, Nitermax)
+    liste_nb_iteration_GS.append(nbiter)
+    nbiter = gauss(A, b)
+    liste_nb_iteration_Gauss.append(nbiter)
 
-
-
-
+print("tailles étudiées : ", taille)
+print("nombre d'itérations Jacobi ", liste_nb_iteration_J)
+print("nombre d'itérations Gauss-Seidel ", liste_nb_iteration_GS)
+print("nombre d'itérations Gauss ", liste_nb_iteration_Gauss)
+ 
+'''
+liste_nb_iteration_Gauss.append(nbiter)
+pp.plot(taille,liste_nb_iteration_J,label = 'Méthode Jacobi')
+pp.plot(taille,liste_nb_iteration_GS,label ='Méthode de Gauss-Seidel')
+pp.plot(taille,liste_nb_iteration_Gauss,label ='Méthode de Gauss')
+pp.xlim(0, 1000)
+pp.xlabel('taille')
+pp.ylabel("nombre d'itérations nécessaires")
+pp.title("Nombre d'itération nécessaires pour plusieurs précisions souhaitées")
+pp.legend()
+pp.show()
+'''
 
